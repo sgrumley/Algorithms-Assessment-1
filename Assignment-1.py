91017e5eccab0131ef331a65a1b78e59ab3fcaaa
Sam Grumley <grumsamu@gmail.com>
2019-04-05 01:52:04 +1000

    Fixed range of iterations

--- Assignment-1.py
+++ Assignment-1.py
@@ -1,5 +1,6 @@
 p = [1,2,3,5,7]
-n = 4
+#p = [1,2,3,5,7]
+n = 3
 total = 8
 limit = 0
 n_val = [0 for i in range(n)]
@@ -11,10 +12,11 @@ shell = 1
 max = 7
 
 
-
-for i in range(20):
+count = 0
+while(1):
     sum = 0
     atLimit = False
+    count +=1
 
     #sum is equal to the sum of p values represented from the n_val array
     for j in range(n):
@@ -24,46 +26,59 @@ for i in range(20):
         if p[n_val[j]] == max:
             atLimit = True
             shell = j
-            #print(j, "needs to increment")
             break
-        print("p value ",j,":", p[n_val[j]])
-        sum += p[n_val[j]]
 
+
+    #if
     if atLimit:
         print("old nval", n_val)
         n_val[shell - 1] += 1
         iters = n-shell
-        for j in range(0, iters+1):
+        print("iters:",iters)
+        for j in range(1, iters+1):
+            print( n_val[shell-1])
             n_val[-j] = n_val[shell-1]
+        atLimit = False
 
+        #if all iterations have been completed
         if n_val[0] == len(p)-1:
             print("we done")
             print("new nval", n_val)
             break
         print("new nval", n_val)
 
-    # find the range for the next possible value
-    limit = total - sum
-    current_val = p[n_val[-1]]
-    next_val = p[n_val[-1]+1]
-    diff = next_val - current_val
+    for j in range(n):
+        if p[n_val[j]] == max:
+            atLimit = True
+            shell = j
 
-    #if soloution
-    if limit == 0:
-        tempSol = []
-        for x in range(n):
-            tempSol.append(p[n_val[x]])
-        print("soloution found", tempSol)
-        exportedSol.append(tempSol)
-        n_val[-2] += 1
-        n_val[-1] = n_val[-2]
-    #if jump in sum is bigger than the remainder needed -> roll back a value and increase
-    elif diff > limit:
-        print("next value doesnt work")
-        n_val[-2] += 1
-        n_val[-1] = n_val[-2]
-    else:
-        n_val[-1] += 1
-    print("Sum: ", sum, " limit: ", limit, "Diff: ", diff)
-    print("------- iteration: ",i+1, "-------" )
+    if atLimit == False:
+        for j in range(n):
+            print("p value ",j,":", p[n_val[j]])
+            sum += p[n_val[j]]
+        # find the range for the next possible value
+        limit = total - sum
+        current_val = p[n_val[-1]]
+        next_val = p[n_val[-1]+1]
+        diff = next_val - current_val
+
+        #if soloution
+        if limit == 0:
+            tempSol = []
+            for x in range(n):
+                tempSol.append(p[n_val[x]])
+            print("soloution found", tempSol)
+            exportedSol.append(tempSol)
+            n_val[-2] += 1
+            n_val[-1] = n_val[-2]
+        #if jump in sum is bigger than the remainder needed -> roll back a value and increase
+        elif diff > limit:
+            print("next value doesnt work")
+            n_val[-2] += 1
+            n_val[-1] = n_val[-2]
+        else:
+            n_val[-1] += 1
+        print("Sum: ", sum, " limit: ", limit, "Diff: ", diff)
+        print("------- iteration: ",count+1, "-------" )
+
 print(exportedSol)
