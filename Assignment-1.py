a773e44b83af2541a319950db2bdcb3e10d73279
Sam <sam@hdmy.com.au>
2019-04-22 23:24:58 +1000

    Fixed CMD print, added CMD filepath input

--- Assignment-1.py
+++ Assignment-1.py
@@ -1,65 +1,70 @@
+"""
+Sam Grumley
+S5048240
+Computing Algorithms
+2801ICT
+Assignment 1
+Breif Description: Write a program that can calculate the total number of ways a given amount can be paid using a specified number of coins (Coins are prime numbers)
+Run: python Assignment-1.py input.txt
+"""
 import time
 import copy
+import sys
 
-def readIn():
-    with open("input.txt") as f:
+""" Reads in Data from file """
+def readIn(fileName):
+    with open(fileName) as f:
         return [line.split() for line in f]
 
 
+""" Writes data to output.txt """
 def writeOut(writeData):
     with open("output.txt", "w") as file:
         for i in range(len(writeData)):
             file.write(str(writeData[i]) + "\n")
 
+
+""" Gets an array of differences between each prime number """
 def differenceArray(data):
     response = []
     for i in range(len(data) -1):
         response.append(data[i+1] - data[i])
     return(response)
 
-def binarySearch(comp, data):
-    left = 0
-    right = len(data) - 1
-    while left <= right:
-        mid = (left + right)//2
-        print("mid", mid)
-        if data[mid] == comp:
-            return True
-        elif data[mid] < comp:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return False
 
-
+""" Check if column is maxed (in n_val) """
 def colCheck(p, n_val):
     max = p[-1]
-    shell = None
+    maxedColPos = None
     atLimit = False
     for j in range(len(n_val)):
-        #check if any column is at its max, record which column
         # This takes the first position from the left
         if p[n_val[j]] == max:
             atLimit = True
-            shell = j
+            maxedColPos = j
             break
-    return atLimit, shell
+    return atLimit, maxedColPos
 
+
+""" Sums prime values based off n_val as an index """
 def summedValues(p, n_val):
     sum = 0
     for j in range(len(n_val)):
         sum += p[n_val[j]]
     return sum
 
+
+""" Gets all prime numbers below total """
 def getPrimeNums(total):
     dataSet = [1,2]
     for i in range (3, total + 1):
         if isPrime(i):
             dataSet.append(i)
         i += 1
-    print(dataSet)
     return dataSet
 
+
+""" Determines if the passed in value is Prime """
 def isPrime(total):
     prime = True
     for j in range(2, total):
@@ -68,6 +73,8 @@ def isPrime(total):
             break
     return prime
 
+
+""" gets the difference between total and the highest prime number """
 def getPrimeGap(total, p):
     primeGap = total - p[-1]
     primeGapList = []
@@ -79,95 +86,88 @@ def getPrimeGap(total, p):
     return primeGap, primeGapList
 
 
+""" Gets all solutions of p that equal total and have n values"""
 def algorithm(n, total, p, difArr):
-    exportedSol = 0
+    numSolutions = 0
     limit = 0
     n_val = [0 for i in range(n)]
-    """while the first value is not maxed out"""
+    #while the first value is not maxed out
     while(n_val[0] != len(p)-1):
         atLimit = False
         #check if a col is maxed
-        atLimit, shell = colCheck(p, n_val)
+        atLimit, maxedColPos = colCheck(p, n_val)
         #use sumcount to find out how many times the nested while runs
         sumcount = 0
         #while a col is maxed increment the column before it"""
         while(atLimit == True):
-            # if shell == all soloutions checked
+            # if maxedColPos == all solutions checked
             sumcount += 1
-            if shell == 0:
+            if maxedColPos == 0:
                 break
             if atLimit:
                 #increment column to the left
-                n_val[shell - 1] += 1
-                iters = n-shell
+                n_val[maxedColPos - 1] += 1
+                iters = n-maxedColPos
                 #set everything on the right of the incremented number equal to the incremented number
                 for j in range(1, iters+1):
-                    n_val[-j] = n_val[shell-1]
+                    n_val[-j] = n_val[maxedColPos-1]
                 atLimit = False
             #check to make sure no other colums are maxed
-            atLimit, shell = colCheck(p, n_val)
+            atLimit, maxedColPos = colCheck(p, n_val)
 
         #compute differences between number needed and next number given
         if atLimit == False:
             # sum values of comination
             sum = summedValues(p, n_val)
-            # find the difference max difference needed to find a soloution
+            # find the difference max difference needed to find a solution
             limit = total - sum
-            #next value - current value
-            #diff = p[n_val[-1]+1] - p[n_val[-1]]
-            #diff = difArr[n_val[-1]]
-            #print("dif by calc",diff)
-            #print("dif by lookup",difArr[n_val[-1]])
-            #print()
-
             #Check if goal state and/or increment
             if limit == 0:
                 tempSol = []
-                exportedSol += 1
+                numSolutions += 1
                 try:
                     n_val[-2] += 1
                     n_val[-1] = n_val[-2]
                 except:
                     n_val[-1] += 1
-                    #pass
             #if jump in sum is bigger than the remainder needed -> roll back a value and increase
             elif difArr[n_val[-1]] > limit:
-                n_val[-2] += 1
-                n_val[-1] = n_val[-2]
+                try:
+                    n_val[-2] += 1
+                    n_val[-1] = n_val[-2]
+                except:
+                    n_val[-1] += 1
             #else keep incrementing the right most column
             else:
                 n_val[-1] += 1
-    return exportedSol
+    return numSolutions
+
 
 """ Controlled function to run a complete cycle of n """
 def runFunc(n, total):
     p = getPrimeNums(total)
-
-    #print("here",difArr)
+    # Return Gold coin Solution
     if n == 1:
-        if total == p[-1]:
-            return 1
-        else:
-            return 0
+        return 1
     # Results for highest prime number
     primeGap, primeGapList = getPrimeGap(total, p)
     difArr = differenceArray(primeGapList)
-    soloutions = algorithm(n-1, primeGap, primeGapList, difArr)
+    Solutions = algorithm(n-1, primeGap, primeGapList, difArr)
     #Results for every other prime number + highest prime number results
     difArr = differenceArray(p)
-    soloutions += algorithm(n, total, p, difArr)
-    print("n = ", n, "soloutions", soloutions)
-    return soloutions
+    Solutions += algorithm(n, total, p, difArr)
+    return Solutions
 
-""" driver """
-readValues = readIn()
+""" Driver """
+fileName = sys.argv[1]
+readValues = readIn(fileName)
 print("read values",readValues)
 printVals = []
+#determine how many values are passed in per line read from input
 for j in range(len(readValues)):
     determineInput = len(readValues[j])
-    print()
-    print(readValues[j])
     start = time.time()
+    #Only one input value e.g. 8
     if determineInput == 1:
         lengthSum = 0
         total = int(readValues[j][0])
@@ -175,17 +175,23 @@ for j in range(len(readValues)):
             length = runFunc(i,total)
             lengthSum += length
         printVals.append(lengthSum)
-        print(time.time() - start)
-        print("total:", lengthSum)
+        print("Input line: ", j+1, " - ",readValues[j] )
+        print("Total solution:", lengthSum)
+        print("time: ",time.time() - start)
+        print()
 
+    # Two input values e.g. 8 2
     elif determineInput == 2:
         total = int(readValues[j][0])
         n = int(readValues[j][1])
         length = runFunc(n, total)
         printVals.append(length)
-        print("total:", length)
-        print(time.time() - start)
+        print("Input line: ", j+1, " - ",readValues[j] )
+        print("Total solution:", lengthSum)
+        print("time: ",time.time() - start)
+        print()
 
+    # Two input values e.g. 8 2 5
     elif determineInput == 3:
         timerSum = 0
         lengthSum = 0
@@ -196,16 +202,9 @@ for j in range(len(readValues)):
             length = runFunc(i,total)
             lengthSum += length
         printVals.append(lengthSum)
-        print(time.time() - start)
-        print("total:", lengthSum)
-print()
-print(printVals)
+        print("Input line: ", j+1, " - ",readValues[j] )
+        print("Total solution:", lengthSum)
+        print("time: ",time.time() - start)
+        print()
+#write output to file
 writeOut(printVals)
-#cn implement difference array
-he = [1,2,3,5,7,11,13,17]
-differenceArray(he)
-
-
-comp = 100
-data = [1,2,3,5,7]
-binarySearch(comp, data)
